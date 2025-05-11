34ca8f00d5c33048554ca75411942a7b9f71e07b
Hein-T.N <heinthetnaung@gmail.com>
2025-05-11 23:30:15 +0800

    Page: Create `Events` page.

---
+++ src/dataset/events.ts
@@ -0,0 +1,22 @@
+export const EventsList = [
+  {
+    imagePath: "/events/event-1.png",
+    title: "Business Talks Shows",
+    description:
+      "Inviting business men as a speakers and assisting them to have a great talk during the whole event while maintaining audience to get a good experience ",
+  },
+  {
+    imagePath: "/events/event-2.png",
+    title: "Running Events",
+    description:
+      "Engage mostly with Gym instructor, Yoga instructor to encourage our customers by not only selling a healthy drinks but also making them to participate it actually and feels the experience how much way our brand can makes them worth drinking it.",
+  },
+  {
+    imagePath: "/events/event-3.png",
+    title: "Product Trade Show",
+    description:
+      "Introducing ourselves to the new customer still by keeping our exisiting customers. Sharing knowledge and having actual feeling of our product and penetrate to the market in one of an effective way",
+  },
+];
+
+
