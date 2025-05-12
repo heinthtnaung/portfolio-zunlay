4d75ac5f4dae6f90cef015836f4faa84723f2126
Hein-T.N <heinthetnaung@gmail.com>
2025-05-12 15:36:38 +0800

    Animation: Add animation in `Events page`.

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,6 @@
 // Components
 import { SimpleCard } from "@/components/Card";
+import PageTransition from "@/components/Transition/pageTransition";
 import { SubTitle, Title } from "@/components/Typography";
 
 // Dataset
@@ -7,24 +8,26 @@ import { EventsList } from "@/dataset/events";
 
 export default function EventsPage() {
   return (
-    <div className="w-screen min-h-screen justify-center bg-white flex items-center overflow-auto">
-      <div className="container mx-auto grid grid-cols-1 gap-4 items-center text-black">
-        <Title text="Events" />
-        <SubTitle
-          text="Different kinds of category sharpen me in event industry."
-          className="font-normal"
-        />
-        <div className="container mx-auto grid grid-cols-3 gap-4 items-start text-black">
-          {EventsList.map((ds, idx) => (
-            <SimpleCard
-              key={"event" + idx}
-              imagePath={ds.imagePath}
-              title={ds.title}
-              description={ds.description}
-            />
-          ))}
+    <PageTransition>
+      <div className="w-screen h-screen justify-center bg-white lg:flex items-center overflow-auto  p-8 ">
+        <div className="container mx-auto grid xl:grid-cols-1 gap-4 items-center text-black lg:pt-0 pt-20">
+          <Title text="Events" />
+          <SubTitle
+            text="Different kinds of category sharpen me in event industry."
+            className="font-normal"
+          />
+          <div className="container mx-auto grid lg:grid-cols-3 gap-4 items-start text-black">
+            {EventsList.map((ds, idx) => (
+              <SimpleCard
+                key={"event" + idx}
+                imagePath={ds.imagePath}
+                title={ds.title}
+                description={ds.description}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </PageTransition>
   );
 }
