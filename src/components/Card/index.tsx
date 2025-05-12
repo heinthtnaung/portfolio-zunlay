e1544ef9992cf9b5474aabc635bdf50a1297a9aa
Hein-T.N <heinthetnaung@gmail.com>
2025-05-12 15:33:14 +0800

    Component: Fix `img tag` issue.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { SubTitle } from "../Typography";
 
 export function SimpleCard(props: {
@@ -8,9 +10,16 @@ export function SimpleCard(props: {
   const { imagePath, title, description } = props;
 
   return (
-    <div className="max-w-md">
-      <img src={imagePath} alt={`image-${title}`} className="rounded-xl h-72" />
-      <SubTitle text={title} className="py-4"/>
+    <div className="max-w-md mx-auto">
+      <picture>
+        <img
+          src={imagePath}
+          alt={`image-${title}`}
+          className="rounded-xl h-72 m-auto"
+        />
+      </picture>
+
+      <SubTitle text={title} className="py-4" />
       <div className="pb-4"> {description} </div>
     </div>
   );
