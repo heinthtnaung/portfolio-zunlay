07c084b36e975b9d391a90573e53e0b1d564608b
Hein-T.N <heinthetnaung@gmail.com>
2025-05-12 15:33:36 +0800

    Animation: Add Animation in `Typography` component.

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import { motion } from "motion/react";
+
 export function Title(props: { text: string; className?: string }) {
   const { text, className } = props;
   return (
-    <div className={`text-7xl font-black ${className}`}>
+    <div
+      className={`text-7xl sm:text-5xl xs:text-5xl font-black ${
+        className ?? ""
+      } relative overflow-hidden`}
+    >
       {text.split("").map((t, idx) => (
-        <span key={`${t + idx}`}> {t} </span>
+        <motion.span
+          key={`${t + idx}`}
+          initial={{ opacity: 0, bottom: "-50px", position: "relative" }}
+          animate={{ opacity: 1, bottom: "0px" }}
+          transition={{
+            duration: 0.2,
+            delay: 0.05 * idx,
+          }}
+        >
+          {t}
+        </motion.span>
       ))}
     </div>
   );
@@ -11,5 +29,46 @@ export function Title(props: { text: string; className?: string }) {
 
 export function SubTitle(props: { text: string; className?: string }) {
   const { text, className } = props;
-  return <div className={`text-2xl font-black ${className}`}> {text} </div>;
+  return (
+    <motion.div
+      className={`text-2xl font-black flex overflow-hidden ${className}`}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visiable"
+    >
+      {text.split(" ").map((t, idx) => (
+        <motion.div
+          className=" relative pr-2"
+          key={`${t + idx}`}
+          variants={itemVariants}
+        >
+          {t + " "}
+        </motion.div>
+      ))}
+    </motion.div>
+  );
 }
+
+/** Animation */
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visiable: {
+    opacity: 1,
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
+const itemVariants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visiable: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.3 },
+  },
+  transition: {
+    duration: 0.1,
+  },
+};
