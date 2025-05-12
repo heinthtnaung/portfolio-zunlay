87fcf07dfc29903d384ee31424863ca56307cb6d
Hein-T.N <heinthetnaung@gmail.com>
2025-05-12 15:36:24 +0800

    Animation: Add animation in `Campaigns page`.

--- src/app/campaigns/page.tsx
+++ src/app/campaigns/page.tsx
@@ -1,26 +1,31 @@
+"use client";
+
 // Components
 import { SimpleCard } from "@/components/Card";
 import { Title } from "@/components/Typography";
 
 // Dataset
 import { CampaignsList } from "@/dataset/campaigns";
+import PageTransition from "@/components/Transition/pageTransition";
 
-export default function EventsPage() {
+export default function CampaignsPage() {
   return (
-    <div className="w-screen min-h-screen justify-center bg-white flex items-center overflow-auto">
-      <div className="container mx-auto grid grid-cols-1 gap-4 items-center text-black">
-        <Title text="Campaigns" className="mb-20" />
-        <div className="container mx-auto grid grid-cols-3 gap-4 items-start text-black">
-          {CampaignsList.map((ds, idx) => (
-            <SimpleCard
-              key={"campaigns" + idx}
-              imagePath={ds.imagePath}
-              title={ds.title}
-              description={ds.description}
-            />
-          ))}
+    <PageTransition>
+      <div className="w-screen h-screen justify-center bg-white lg:flex items-center overflow-auto overflow-x-hidden p-8">
+        <div className="container mx-auto grid grid-cols-1 gap-4 items-center text-black lg:pt-0 pt-20">
+          <Title text="Campaigns" className="mb-20" />
+          <div className="container mx-auto grid lg:grid-cols-3 gap-4 items-start text-black">
+            {CampaignsList.map((ds, idx) => (
+              <SimpleCard
+                key={"campaigns" + idx}
+                imagePath={ds.imagePath}
+                title={ds.title}
+                description={ds.description}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </PageTransition>
   );
 }
